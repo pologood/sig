d50a93242b815cf2695e5f3f89c23022ba504b52
skgod37 <dhshuai@gmail.com>
2014-07-17 15:40:45 +0800

    add framework

--- sig-core/src/main/java/org/atlantis/core/dao/impl/base/MongodbTemplate.java
+++ sig-core/src/main/java/org/atlantis/core/dao/impl/base/MongodbTemplate.java
@@ -2,7 +2,6 @@ package org.atlantis.core.dao.impl.base;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
