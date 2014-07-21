6ae4ac3b7e7a67bbc1632d4668c44bdeba80dcf9
skgod37 <dhshuai@gmail.com>
2014-07-21 16:00:25 +0800

    change webapp package name

---
+++ sig-web/src/main/java/org/atlantis/web/action/PostManageAction.java
@@ -0,0 +1,78 @@
+package org.atlantis.web.action;
+
+import java.util.List;
+
+import org.atlantis.web.Form.Query;
+import org.sig.common.DwrResult;
+import org.sig.core.bo.Post;
+import org.sig.core.bo.PostClass;
+import org.sig.core.service.impl.PostManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *  @Copyright :    All right reserved @ Sogou, Inc.
+ *  @CreateTime :  2013-3-13  下午3:38:01 
+ *  @Author :          Deng Hongshuai
+ *  @Description:   
+ *
+ **/
+@Component("PostManageAction")
+public class PostManageAction {
+	
+	@Autowired
+	private PostManageService postManageService;
+	
+	/**
+	 * Add a new Post
+	 * @param post
+	 * @return
+	 */
+	public  DwrResult addPost(Post post){
+		DwrResult result = new DwrResult();
+		
+	   try {
+		   postManageService.addPost(post);
+		   
+		   result.setSuccess(true);
+		   result.setMessage("Add a post successfully");
+	   } catch (Exception e){
+		   result.setSuccess(false);
+		   result.setMessage(e.getMessage());
+		   e.printStackTrace();
+	   } 
+		   
+	return  result;
+	
+	}
+	
+	
+	/**
+	 * Update a post
+	 * @param post
+	 * @return
+	 */
+	public DwrResult updatePost(Query query){
+		return null;
+	}
+	
+	/**
+	 * Delete a post
+	 * @param postId
+	 * @return
+	 */
+	public DwrResult delPost(Query query){
+		return null;
+	}
+	
+	/**
+	 * Query posts
+	 * @param query
+	 * @return
+	 */
+	public DwrResult getPost(Query query){
+		return null;
+	}
+	
+
+}
---
+++ sig-web/src/main/java/org/atlantis/web/taglib/AuthChecker.java
@@ -0,0 +1,60 @@
+package org.atlantis.web.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.commons.lang.StringUtils;
+
+
+public class AuthChecker extends TagSupport {
+	private static final long serialVersionUID = 339306968881056890L;
+
+	private String authid;
+
+	public AuthChecker() {
+	}
+
+	public int doStartTag() throws JspException {
+		if (condition()) {
+			return (EVAL_BODY_INCLUDE);
+		} else {
+			return (SKIP_BODY);
+		}
+
+	}
+
+	/**
+	 * condition
+	 * 
+	 * @return boolean
+	 */
+	private boolean condition() {
+		
+//		boolean ifAuthCheck = false;
+//		User user = (User) pageContext.getSession().getAttribute(WebUtil.USER_IN_SESSION);
+//		if(authid.indexOf("||") != -1) {
+//			for(String id:StringUtils.split(authid, "||")) {
+//				if(user.validateAuth(id.trim())) {
+//					ifAuthCheck = true;
+//					break;
+//				}
+//			}
+//		}else {
+//			ifAuthCheck = user.validateAuth(authid);
+//		}
+		return true;
+	}
+
+	public int doEndTag() throws JspException {
+		return (EVAL_PAGE);
+	}
+
+	public String getAuthid() {
+		return authid;
+	}
+
+	public void setAuthid(String authid) {
+		this.authid = authid;
+	}
+
+}
