2133d1279cdf7ef831a4291d82faa6865e2c71e0
skgod37 <dhshuai@gmail.com>
2014-07-25 15:59:09 +0800

    add attribute

---
+++ sig-common/src/main/java/org/atlantis/common/enums/Attribute.java
@@ -0,0 +1,13 @@
+package org.atlantis.common.enums;
+
+/**
+ * @copyright  www.sogou.com
+ * @author       denghongshuai@sogou-inc.com
+ * @time           2014-7-25   下午3:58:53
+ * @version    
+ *
+ * @description 
+ */
+public enum Attribute {
+
+}
